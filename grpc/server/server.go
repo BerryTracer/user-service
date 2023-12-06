c2c8bd74b5f352f5920cbb112934c4d005a95462
JonathanInTheClouds <48375298+JonathanInTheClouds@users.noreply.github.com>
2023-12-06 13:45:35 -0600

    🔧 fix(server.go): update import statement for proto package to improve readability and maintain consistency 🚀 feat(server.go): update method signatures to use proto package types for better compatibility with gRPC

--- grpc/server/server.go
+++ grpc/server/server.go
@@ -5,14 +5,14 @@ import (
 	"log"
 	"net"
 
-	user_service "github.com/BerryTracer/user-service/grpc/proto"
+	proto "github.com/BerryTracer/user-service/grpc/proto"
 	"github.com/BerryTracer/user-service/service"
 	"google.golang.org/grpc"
 )
 
 type UserGRPCServer struct {
 	UserService service.UserService
-	user_service.UnimplementedUserServiceServer
+	proto.UnimplementedUserServiceServer
 }
 
 func NewUserGRPCServer(userService service.UserService) *UserGRPCServer {
@@ -28,7 +28,7 @@ func (s *UserGRPCServer) Run(port string) error {
 		return err
 	}
 	server := grpc.NewServer()
-	user_service.RegisterUserServiceServer(server, s)
+	proto.RegisterUserServiceServer(server, s)
 	log.Printf("gRPC server listening on port %s\n", port)
 	if err := server.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v\n", err)
@@ -37,7 +37,7 @@ func (s *UserGRPCServer) Run(port string) error {
 	return nil
 }
 
-func (s *UserGRPCServer) GetUserById(ctx context.Context, req *user_service.GetUserRequest) (*user_service.User, error) {
+func (s *UserGRPCServer) GetUserById(ctx context.Context, req *proto.GetUserRequest) (*proto.User, error) {
 	user, err := s.UserService.GetUserById(ctx, req.GetId())
 	if err != nil {
 		log.Fatalf("failed to get user by id: %v\n", err)
@@ -47,7 +47,7 @@ func (s *UserGRPCServer) GetUserById(ctx context.Context, req *user_service.GetU
 	return user.ConvertToProto(), nil
 }
 
-func (s *UserGRPCServer) GetUserByEmail(ctx context.Context, req *user_service.GetUserByEmailRequest) (*user_service.User, error) {
+func (s *UserGRPCServer) GetUserByEmail(ctx context.Context, req *proto.GetUserByEmailRequest) (*proto.User, error) {
 	user, err := s.UserService.GetUserByEmail(ctx, req.GetEmail())
 	if err != nil {
 		log.Fatalf("failed to get user by email: %v\n", err)
@@ -57,7 +57,7 @@ func (s *UserGRPCServer) GetUserByEmail(ctx context.Context, req *user_service.G
 	return user.ConvertToProto(), nil
 }
 
-func (s *UserGRPCServer) GetUserByUsername(ctx context.Context, req *user_service.GetUserByUsernameRequest) (*user_service.User, error) {
+func (s *UserGRPCServer) GetUserByUsername(ctx context.Context, req *proto.GetUserByUsernameRequest) (*proto.User, error) {
 	user, err := s.UserService.GetUserByUsername(ctx, req.GetUsername())
 	if err != nil {
 		log.Fatalf("failed to get user by username: %v\n", err)
@@ -67,7 +67,7 @@ func (s *UserGRPCServer) GetUserByUsername(ctx context.Context, req *user_servic
 	return user.ConvertToProto(), nil
 }
 
-func (s *UserGRPCServer) CreateUser(ctx context.Context, req *user_service.CreateUserRequest) (*user_service.User, error) {
+func (s *UserGRPCServer) CreateUser(ctx context.Context, req *proto.CreateUserRequest) (*proto.User, error) {
 	user, err := s.UserService.CreateUser(ctx, req.GetUsername(), req.GetEmail(), req.GetPassword())
 	if err != nil {
 		log.Fatalf("failed to create user: %v\n", err)
